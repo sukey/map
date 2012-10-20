4c25ffc0271b5b38bcbbe5233568756056ea207c
root <root@hartley.lan.hack.rs>
2012-10-20 14:30:41 +0100

    updated the JSON

--- geojson.js
+++ geojson.js
@@ -4,55 +4,6 @@ var converted_json = {
 { "type": "Feature", "properties": { "Name": "Start point", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.111494, 51.510986, 0.000000 ] } }
 ,
 { "type": "Feature", "properties": { "Name": "route", "Description": "" }, "geometry": { "type": "LineString", "coordinates": [ [ -0.116810, 51.510391 ], [ -0.120160, 51.509270 ], [ -0.122300, 51.506710 ], [ -0.124110, 51.500992 ], [ -0.126420, 51.501099 ], [ -0.126340, 51.504471 ], [ -0.126980, 51.506020 ], [ -0.127970, 51.507462 ], [ -0.129170, 51.507519 ], [ -0.130950, 51.507702 ], [ -0.132690, 51.507240 ], [ -0.133710, 51.508530 ], [ -0.134960, 51.509731 ], [ -0.141700, 51.507561 ], [ -0.145910, 51.505421 ], [ -0.150290, 51.503448 ], [ -0.151490, 51.503712 ], [ -0.160589, 51.508743 ] ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Rally main stage", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.160632, 51.508663, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "toilets", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.127802, 51.507633, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "short march assembly point", "Description": "<div dir=\"ltr\">This is where those who are not able to march the full length of the route should assemble.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.140204, 51.507179, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Assembly point for marchers with disabilities", "Description": "<div dir=\"ltr\">Contact us for access details - http:\/\/afuturethatworks.org<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.119047, 51.509865, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "form up", "Description": "" }, "geometry": { "type": "LineString", "coordinates": [ [ -0.116730, 51.510399 ], [ -0.109949, 51.511307 ], [ -0.104370, 51.510986 ] ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Rear of march formation", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.104027, 51.510933, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Blackfriars station", "Description": "Best tube\/train location for joining the march from the rear, and avoiding congestion and delay.<br>" }, "geometry": { "type": "Point", "coordinates": [ -0.103469, 51.510986, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Parliament Square", "Description": "<br>" }, "geometry": { "type": "Point", "coordinates": [ -0.126536, 51.500820, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Downing Street", "Description": "<br>" }, "geometry": { "type": "Point", "coordinates": [ -0.126525, 51.503166, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Trafalgar Square", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.128081, 51.508015, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Picadilly Circus", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.134722, 51.509972, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Wellington Arch", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.150976, 51.502773, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Westminster Bridge", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.123682, 51.500809, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Waterloo Bridge", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.117803, 51.509678, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Temple Station closed", "Description": "<div dir=\"ltr\">Temple Station will be closed on the day, due to heavy crowds expected at the start of the march.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.114326, 51.510933, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Embankment station busy", "Description": "<div dir=\"ltr\">Embankment station will be very crowded on the day. We suggest avoiding it if possible.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.122652, 51.506874, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Westminster Station busy", "Description": "<div dir=\"ltr\">Westminster station will be very crowded on the day. We suggest avoiding it if possible.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.125012, 51.501102, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Green Park Station busy", "Description": "<div dir=\"ltr\">Access to and from Green Park station may be crowded on the day. <\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.142951, 51.506741, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Hyde Park Station", "Description": "<div dir=\"ltr\">Hyde Park station will be very crowded later on the day, especially as people leave the rally. We suggest finding another route if possible.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.152221, 51.502865, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Blackfriars Millennium Pier", "Description": "<div dir=\"ltr\">Closest riverboat pier for people joining the march by boat.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.105625, 51.510658, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Cleopatra's Needle", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.120270, 51.508335, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "Lambeth Pier", "Description": "<div dir=\"ltr\">Embark here for free boats to Blackfriars' Pier. Running every 10 minutes from 10am to 1pm.<\/div>" }, "geometry": { "type": "Point", "coordinates": [ -0.121171, 51.495144, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "toilets", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.159431, 51.506927, 0.000000 ] } }
-,
-{ "type": "Feature", "properties": { "Name": "toilets", "Description": "" }, "geometry": { "type": "Point", "coordinates": [ -0.159259, 51.510372, 0.000000 ] } }
-
 ]
 } ;
 
