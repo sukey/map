94b6b15dcfe7afc8a55476cf0b14d55587f2b37b
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-10-20 04:25:50 -0700

    added some more wifi hotspots

--- geojson.js
+++ geojson.js
@@ -2,13 +2,13 @@ var fsWifi = {
     "type": "FeatureCollection",
     "features": [
         {
-	    "type": "Feature",
-	    "properties": {
-		"popupContent": "wifi"
-	    },
-	"geometry": {
-		"type": "Point",
-		"coordinates": [-0.087, 51.520]} //this is staburcks near FS
+   "type": "Feature",
+   "properties": {
+    "popupContent": "wifi"
+   },
+   "geometry": {
+   "type": "Point",
+   "coordinates": [-0.087, 51.520]} //this is staburcks near FS
         },{
             "type": "Feature",
             "properties": {
@@ -18,6 +18,105 @@ var fsWifi = {
                 "type": "Point",
                 "coordinates": [-0.125321, 51.508805]
             }
-        }
+        }, {
+            "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.139195, 51.508304]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.12532, 51.508805]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.09619, 51.51461]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.088094, 51.512622]
+            }
+  }, {
+      "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.089882,51.504657]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.098723,51.506692]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.117878,51.502352]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.113865,51.51834]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.118861,51.519988]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.097647,51.522516]
+            }
+  }, {
+   "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.087879,51.526342]
+            }
+  }
     ]
 };
