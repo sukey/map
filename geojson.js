a0429f132dcb73c31d36219c9d7bf8dcf04c41bc
Samuel Carlisle <samuelcarlisle@gmail.com>
2012-10-20 04:06:35 -0700

    first commit

---
+++ geojson.js
@@ -0,0 +1,23 @@
+var fsWifi = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+	    "type": "Feature",
+	    "properties": {
+		"popupContent": "wifi"
+	    },
+	"geometry": {
+		"type": "Point",
+		"coordinates": [-0.087, 51.520]} //this is staburcks near FS
+        },{
+            "type": "Feature",
+            "properties": {
+                "popupContent": "wifi"
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [-0.125321, 51.508805]
+            }
+        }
+    ]
+};
